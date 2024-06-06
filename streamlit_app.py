4d71f9eb3041d547dafae4fc9ba49b02f6ec0d3f
gassala <166111919+gassala@users.noreply.github.com>
2024-06-06 14:42:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
-st.title('Calculate Square Area')
+st.title('Area Calculator')
 
 s=st.number_input ("Input the side",0)
-count=st.button ("Calculate the area")
+count=st.button ("Calculate")
 if count:
   area=s*s
   st.write("The square area= ", area)
+  st.succers(f('"The square area= {area}"))
 
